cf64262ac073428f3b560742f747a26e267f5ed1
phea3 <sokphealoun9@gmail.com>
2026-06-16 18:19:31 +0700

    update

---
+++ app/api/user-id/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from "next/server";
+import { userSchema } from "@/db/schema";
+import { db } from "@/db/indext";
+import { eq } from "drizzle-orm";
+
+export async function GET(req: NextRequest) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const id = searchParams.get("id");
+
+    if (!id) {
+      return NextResponse.json(
+        { ok: false, message: "Missing id parameter" },
+        { status: 400 },
+      );
+    }
+
+    const user = await db.query.userSchema.findFirst({
+      where: eq(userSchema.id, id),
+    });
+
+    return NextResponse.json({
+      ok: true,
+      data: user,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Failed to fetch guides",
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+}
