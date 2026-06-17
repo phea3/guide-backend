67427d0d5d9dd684005b82dc7a3e543432d354d9
phea3 <sokphealoun9@gmail.com>
2026-06-17 11:41:38 +0700

    update

--- db/indext.ts
+++ db/indext.ts
@@ -1,9 +1,19 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/mysql2";
+import mysql from "mysql2/promise";
 
 import * as schema from "./schema";
 
-export const db = drizzle(process.env.DATABASE_URL!, {
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT ?? 3310),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+export const db = drizzle({
+  client: pool,
   schema,
   mode: "default",
 });
